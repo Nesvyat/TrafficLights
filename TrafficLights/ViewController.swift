104f15b53d7244847d589b23e2b98230f2022856
Nesvyat <nesvyat777@gmail.com>
2022-08-03 17:51:59 +0300

    1 Alpha =0.3 2 corners radius = 45

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            redColorView.layer.cornerRadius = 45
+            yellowColorView.layer.cornerRadius = 45
+            greenColorView.layer.cornerRadius = 45
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 0.3
+            greenColorView.alpha = 0.3
+            
+        startButton.tintColor = .gray
+        startButton.backgroundColor = .blue
+        startButton.layer.cornerRadius = 20
+        
+            
+        }
+
+    @IBAction func startButtonPressed() {
+        
+        
     }
-
-
 }
+
+
+
 
