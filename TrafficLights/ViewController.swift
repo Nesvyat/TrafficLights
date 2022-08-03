6c0a7ea31ed63fa7ae8c3fa5710ec86ea86677c7
Nesvyat <nesvyat777@gmail.com>
2022-08-03 18:21:40 +0300

    И вот здесь не понимаю, что нужно писать

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
-    
+    private let lightIsOn = 1
+    private let lightIsOff = 0.3
     
     
     override func viewDidLoad() {
@@ -24,22 +25,45 @@ class ViewController: UIViewController {
             redColorView.layer.cornerRadius = 45
             yellowColorView.layer.cornerRadius = 45
             greenColorView.layer.cornerRadius = 45
-            redColorView.alpha = 0.3
-            yellowColorView.alpha = 0.3
-            greenColorView.alpha = 0.3
+           // redColorView.alpha = 0.3
+            //yellowColorView.alpha = 0.3
+            //greenColorView.alpha = 0.3       и вот это не нужно как я понимаю , я еще плюсом в менюшке поменял на 0.3
             
         startButton.tintColor = .gray
         startButton.backgroundColor = .blue
         startButton.layer.cornerRadius = 20
         
-            
+        redColorView.alpha = lightIsOff //вот тут не понял , откуда это , я смотрел разбор и по памяти сделал, все понятно кроме этого lightOff имею в виду
+        yellowColorView.alpha = lightIsOff
+        greenColorView.alpha = lightIsOff
         }
 
     @IBAction func startButtonPressed() {
         
-        
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("Next", for: .normal)
+    
     }
-}
+        if redColorView.alpha == 1 {
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 1
+        } else if yellowColorView.alpha == 1 {
+                yellowColorView.alpha = 0.3
+                greenColorView.alpha = 1
+        } else if greenColorView.alpha == 1 {
+            greenColorView.alpha = 0.3
+            redColorView.alpha = 1
+        } else {
+            redColorView.alpha = 1
+            
+            
+                    
+                    
+                    }
+                }
+            }
+        
+
 
 
 
